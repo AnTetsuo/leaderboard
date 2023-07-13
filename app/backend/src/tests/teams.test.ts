f105b4166fe483864aba8b27e86cd35a46dde2f9
Andrey Tetsuo <tetsuo_miyasato@hotmail.com>
2023-07-13 18:47:25 -0300

    tests(teams): get @ "/" scenario

---
+++ app/backend/src/tests/teams.test.ts
@@ -0,0 +1,25 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+import * as mock from './mocks/teams.mock'
+import { app } from '../app';
+
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Fluxo TEAMS ', () => {
+  beforeEach(async () => { sinon.restore() } );
+
+  it('00- GET @ "/" => returns the team list', async () => {
+    //ARRANGE
+    sinon.stub(teamsModel, 'listAll').resolves(mock.list);
+    //ACT
+    const response = await chai.request(app).get('/teams')
+    //ASSERT
+    expect(response.status).to.eq(200);
+    expect(response.body).to.be.deep.equal(mock.list);
+  });
+});
