c1772650d990f76c069ace9f59476fc9114d6245
Andrey Tetsuo <tetsuo_miyasato@hotmail.com>
2023-07-13 16:09:45 -0300

    feat(teams): Initial migration and model setup

---
+++ app/backend/src/database/models/Team.model.ts
@@ -0,0 +1,32 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+export default class Team extends Model<InferAttributes<Team>,
+InferCreationAttributes<Team>> {
+  declare id: CreationOptional<number>;
+  declare team_name: string;
+}
+
+Team.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  team_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
+}, {
+  sequelize: db,
+  timestamps: false,
+  tableName: 'teams',
+});
