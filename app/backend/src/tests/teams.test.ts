d317ed56ca98a01aabbabe3e40caf4d7f0fd78d4
Andrey Tetsuo <tetsuo_miyasato@hotmail.com>
2023-07-13 23:36:47 -0300

    tests(teams): get @ /teams/:id scenario

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -10,16 +10,56 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
+const sww = 'Something went wrong';
+
 describe('Fluxo TEAMS ', () => {
   beforeEach(async () => { sinon.restore() } );
 
-  it('00- GET @ "/" => returns the team list', async () => {
-    //ARRANGE
-    sinon.stub(TeamModel.prototype, 'listAll').resolves(mock.list);
-    //ACT
-    const response = await chai.request(app).get('/teams')
-    //ASSERT
-    expect(response.status).to.eq(200);
-    expect(response.body).to.be.deep.equal(mock.list);
-  });
+  describe('GET /', function () {
+    it('00- SUCCESS => returns the team list', async function() {
+      sinon.stub(TeamModel.prototype, 'listAll').resolves(mock.list);
+
+      const response = await chai.request(app).get('/teams');
+
+      expect(response.status).to.eq(200);
+      expect(response.body).to.be.deep.equal(mock.list);
+    });
+
+    it('01- THROWS => on db error returns status 500 and sww message', async function () {
+      sinon.stub(TeamModel.prototype, 'listAll').throws();
+
+      const response = await chai.request(app).get('/teams')
+
+      expect(response.status).to.eq(500);
+      expect(response.body).to.be.deep.equal({ message: sww });
+    })
+  })
+
+  describe('GET "/:id"', function () {
+    it('00- SUCCESS', async function () {
+      sinon.stub(TeamModel.prototype, 'findById').resolves(mock.list[0]);
+
+      const response = await chai.request(app).get('/teams/1');
+
+      expect(response.status).to.eq(200);
+      expect(response.body).to.deep.equal(mock.list[0]);
+    });
+    it('01- FAILURE - NOT FOUND', async function() {
+      sinon.stub(TeamModel.prototype, 'findById').resolves(null);
+
+      const response = await chai.request(app).get('/teams/1');
+
+      expect(response.status).to.eq(404);
+      expect(response.body).to.deep.equal({ message: 'Team not found' });
+    });
+    it('02- THROWS', async function() {
+      sinon.stub(TeamModel.prototype, 'findById').throws();
+
+      const response = await chai.request(app).get('/teams/1');
+
+      expect(response.status).to.eq(200);
+      expect(response.body).to.be.deep.equal(sww);
+    });
+  })
+
 });
