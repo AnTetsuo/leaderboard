85c31d95b24e00eeb1a819908d289075075053cb
Andrey Tetsuo <tetsuo_miyasato@hotmail.com>
2023-07-18 16:15:00 -0300

    tests(matches): add tests for patch @ matches/:id/finish - matches/:id endpoints

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -2,7 +2,9 @@ import * as sinon from 'sinon';
 import * as chai from 'chai';
 // @ts-ignore
 import chaiHttp = require('chai-http');
-import * as mock from './mocks/matches.mock'
+import * as mock from './mocks/matches.mock';
+import * as authMock from './mocks/login.mock';
+import jwt from '../utils/jwt';
 import { app } from '../app';
 import MatchesModel from '../database/models/useModels/MatchesModel';
 
@@ -26,7 +28,6 @@ describe('Fluxo MATCHES ', () => {
     });
 
     it('01- SUCCESS => returns the in progress matches when query term "inProgress" is "true"', async function () {
-      
       sinon.stub(MatchesModel.prototype, 'getAll').resolves(mock.InProgress);
 
       const response = await chai.request(app).get('/matches?inProgress=true');
@@ -36,7 +37,6 @@ describe('Fluxo MATCHES ', () => {
     })
 
     it('02- SUCCESS => returns the finished matches when query term "inProgress" is "false"', async function () {
-      
       sinon.stub(MatchesModel.prototype, 'getAll').resolves(mock.InProgress);
 
       const response = await chai.request(app).get('/matches?inProgress=true');
@@ -63,4 +63,62 @@ describe('Fluxo MATCHES ', () => {
       expect(response.body).to.be.deep.equal({ message: sww });
     })
   })
+
+  describe('PATCH /:id/finish', function() {
+    it('00- SUCCESS => returns message with 1 on data field', async function () {
+      sinon.stub(MatchesModel.prototype, 'finishMatch').resolves(1);
+      sinon.stub(jwt, 'verify').returns(authMock.user);
+
+      const response = await chai.request(app)
+        .patch('/matches/1/finish')
+        .set('authorization', authMock.authHeader);
+
+      expect(response.status).to.be.eq(200);
+      expect(response.body).to.be.eq(1);
+    })
+
+    it('01- THROWS => returns the status 500 if db fails', async function () {
+      sinon.stub(MatchesModel.prototype, 'finishMatch').throws();
+
+      const response = await chai.request(app)
+      .patch('/matches/1/finish')
+      .set('authorization', authMock.authHeader);
+
+      expect(response.status).to.be.eq(500);
+      expect(response.body).to.deep.equal({ message: sww });
+    })
+  })
+
+  describe('PATCH /:id', function() {
+    it('00- SUCCESS => returns 1 indicating the number of affected rows', async function () {
+      sinon.stub(MatchesModel.prototype, 'patchMatch').resolves(1);
+      sinon.stub(jwt, 'verify').returns(authMock.user);
+
+      const response = await chai.request(app)
+        .patch('/matches/1')
+        .set('authorization', authMock.authHeader)
+        .send({
+          homeTeamGoals: 1,
+          awayTeamGoals: 2,
+        });
+      
+      expect(response.status).to.be.eq(200);
+      expect(response.body).to.have.property('message', 'OK');
+    })
+
+    it('01- THROWS => returns the status 500 if db fails', async function () {
+      sinon.stub(MatchesModel.prototype, 'patchMatch').throws();
+
+      const response = await chai.request(app)
+      .patch('/matches/1')
+      .set('authorization', authMock.authHeader)
+      .send({
+        homeTeamGoals: 1,
+        awayTeamGoals: 2,
+      });;
+
+      expect(response.status).to.be.eq(500);
+      expect(response.body).to.deep.equal({ message: sww });
+    })
+})
 });
