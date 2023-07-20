ab0e25c31bc57124fb0ffbe321c414c95d9eb18b
Andrey Tetsuo <tetsuo_miyasato@hotmail.com>
2023-07-20 16:48:54 -0300

    tests(leaderboard): get @ /leaderboard/home and /leanderboard/away routes

---
+++ app/backend/src/tests/leaderboard.test.ts
@@ -0,0 +1,46 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+import * as mock from './mocks/leaderboard.mock'
+import { app } from '../app';
+import MatchesModel from '../database/models/useModels/MatchesModel';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+const sww = 'Something went wrong';
+
+describe('Fluxo LEADERBOARD ', () => {
+  beforeEach(async () => { sinon.restore() } );
+
+  describe('GET /leaderboard/home || /leaderboard/away', function () {
+    it('00- SUCCESS => returns the leaderboard ranking on Home Games', async function() {
+      sinon.stub(MatchesModel.prototype, 'getAll').resolves(mock.matches);
+
+      const response = await chai.request(app).get('/leaderboard/home');
+
+      expect(response.status).to.eq(200);
+      expect(response.body).to.be.deep.equal(mock.boardHome);
+    });
+
+    it('01- SUCCESS => returns the leaderboard ranking on Away Games', async function() {
+      sinon.stub(MatchesModel.prototype, 'getAll').resolves(mock.matches);
+
+      const response = await chai.request(app).get('/leaderboard/away');
+
+      expect(response.status).to.eq(200);
+      expect(response.body).to.be.deep.equal(mock.boardAway);
+    });
+
+    it('02- THROWS => on db error returns status 500 and sww message', async function () {
+      sinon.stub(MatchesModel.prototype, 'getAll').throws();
+
+      const response = await chai.request(app).get('/leaderboard/home')
+
+      expect(response.status).to.eq(500);
+      expect(response.body).to.be.deep.equal({ message: sww });
+    })
+  })
+});
---
+++ app/backend/src/tests/mocks/leaderboard.mock.ts
@@ -0,0 +1,217 @@
+export const matches = [
+  {
+		"id": 1,
+		"homeTeamId": 16,
+		"homeTeamGoals": 1,
+		"awayTeamId": 9,
+		"awayTeamGoals": 1,
+		"inProgress": false,
+		"homeTeam": {
+			"teamName": "São Paulo"
+		},
+		"awayTeam": {
+			"teamName": "Grêmio"
+		}
+	},
+	{
+		"id": 2,
+		"homeTeamId": 9,
+		"homeTeamGoals": 2,
+		"awayTeamId": 16,
+		"awayTeamGoals": 0,
+		"inProgress": false,
+		"homeTeam": {
+			"teamName": "Grêmio"
+		},
+		"awayTeam": {
+			"teamName": "São Paulo"
+		}
+	},
+  {
+		"id": 3,
+		"homeTeamId": 16,
+		"homeTeamGoals": 1,
+		"awayTeamId": 9,
+		"awayTeamGoals": 1,
+		"inProgress": false,
+		"homeTeam": {
+			"teamName": "São Paulo"
+		},
+		"awayTeam": {
+			"teamName": "Grêmio"
+		}
+	},
+	{
+		"id": 4,
+		"homeTeamId": 9,
+		"homeTeamGoals": 2,
+		"awayTeamId": 16,
+		"awayTeamGoals": 0,
+		"inProgress": false,
+		"homeTeam": {
+			"teamName": "Grêmio"
+		},
+		"awayTeam": {
+			"teamName": "São Paulo"
+		}
+	},
+  {
+		"id": 5,
+		"homeTeamId": 3,
+		"homeTeamGoals": 1,
+		"awayTeamId": 5,
+		"awayTeamGoals": 1,
+		"inProgress": false,
+		"homeTeam": {
+			"teamName": "C9"
+		},
+		"awayTeam": {
+			"teamName": "Navi"
+		}
+	},
+	{
+		"id": 6,
+		"homeTeamId": 5,
+		"homeTeamGoals": 2,
+		"awayTeamId": 3,
+		"awayTeamGoals": 0,
+		"inProgress": false,
+		"homeTeam": {
+			"teamName": "Navi"
+		},
+		"awayTeam": {
+			"teamName": "C9"
+		}
+	},
+  {
+		"id": 7,
+		"homeTeamId": 3,
+		"homeTeamGoals": 1,
+		"awayTeamId": 5,
+		"awayTeamGoals": 1,
+		"inProgress": false,
+		"homeTeam": {
+			"teamName": "C9"
+		},
+		"awayTeam": {
+			"teamName": "Navi"
+		}
+	},
+	{
+		"id": 8,
+		"homeTeamId": 5,
+		"homeTeamGoals": 2,
+		"awayTeamId": 3,
+		"awayTeamGoals": 0,
+		"inProgress": false,
+		"homeTeam": {
+			"teamName": "Navi"
+		},
+		"awayTeam": {
+			"teamName": "C9"
+		}
+	}
+]
+
+export const boardAway = [
+  {
+    "name": "Grêmio",
+    "totalPoints": 2,
+    "totalGames": 2,
+    "totalVictories": 0,
+    "totalDraws": 2,
+    "totalLosses": 0,
+    "goalsFavor": 2,
+    "goalsOwn": 2,
+    "goalsBalance": 0,
+    "efficiency": 33.33
+  },
+  {
+    "name": "Navi",
+    "totalPoints": 2,
+    "totalGames": 2,
+    "totalVictories": 0,
+    "totalDraws": 2,
+    "totalLosses": 0,
+    "goalsFavor": 2,
+    "goalsOwn": 2,
+    "goalsBalance": 0,
+    "efficiency": 33.33
+  },
+  {
+    "name": "São Paulo",
+    "totalPoints": 0,
+    "totalGames": 2,
+    "totalVictories": 0,
+    "totalDraws": 0,
+    "totalLosses": 2,
+    "goalsFavor": 0,
+    "goalsOwn": 4,
+    "goalsBalance": -4,
+    "efficiency": 0
+  },
+  {
+    "name": "C9",
+    "totalPoints": 0,
+    "totalGames": 2,
+    "totalVictories": 0,
+    "totalDraws": 0,
+    "totalLosses": 2,
+    "goalsFavor": 0,
+    "goalsOwn": 4,
+    "goalsBalance": -4,
+    "efficiency": 0
+  },
+]
+
+
+export const boardHome = [
+    {
+      "name": "Grêmio",
+      "totalPoints": 6,
+      "totalGames": 2,
+      "totalVictories": 2,
+      "totalDraws": 0,
+      "totalLosses": 0,
+      "goalsFavor": 4,
+      "goalsOwn": 0,
+      "goalsBalance": 4,
+      "efficiency": 100
+    },
+    {
+      "name": "Navi",
+      "totalPoints": 6,
+      "totalGames": 2,
+      "totalVictories": 2,
+      "totalDraws": 0,
+      "totalLosses": 0,
+      "goalsFavor": 4,
+      "goalsOwn": 0,
+      "goalsBalance": 4,
+      "efficiency": 100
+    },
+    {
+      "name": "São Paulo",
+      "totalPoints": 2,
+      "totalGames": 2,
+      "totalVictories": 0,
+      "totalDraws": 2,
+      "totalLosses": 0,
+      "goalsFavor": 2,
+      "goalsOwn": 2,
+      "goalsBalance": 0,
+      "efficiency": 33.33
+    },
+    {
+      "name": "C9",
+      "totalPoints": 2,
+      "totalGames": 2,
+      "totalVictories": 0,
+      "totalDraws": 2,
+      "totalLosses": 0,
+      "goalsFavor": 2,
+      "goalsOwn": 2,
+      "goalsBalance": 0,
+      "efficiency": 33.33
+    },
+]
